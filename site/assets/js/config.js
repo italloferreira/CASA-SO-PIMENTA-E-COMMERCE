7ff45b93d705d1484d7ac202d2eaeb64522813b2
Itallo Ferreira <italloferreira2208@gmail.com>
2026-07-06 19:22:13 -0300

    chore: update API URL to production (Render)

--- site/assets/js/config.js
+++ site/assets/js/config.js
@@ -2,4 +2,4 @@
 /* Troque a URL de producao antes do deploy */
 var API_BASE_URL = (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1')
   ? 'http://localhost:3333'
-  : 'https://SUBSTITUA_PELA_URL_DE_PRODUCAO';
+  : 'https://casa-so-pimenta-api.onrender.com';
